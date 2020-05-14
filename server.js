ff6e7dd036d913d0b36c58f610bd9af8d06d6fe7
MV-stack <59325046+MV-stack@users.noreply.github.com>
2020-05-13 21:16:23 -0700

    corrected typo in server.js route for api-routes.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 
 // Routes
 require("./routes/html-routes.js")(app);
-require("./routes/post-api-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
