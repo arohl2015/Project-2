797aeacadcbd9a66bf916dde543ca7f68918a954
A Rohl <april.rohl2015@gmail.com>
2020-05-18 19:40:56 -0700

    passport.js updates to files

---
+++ config/passport.js
@@ -0,0 +1,24 @@
+// most of the below code was provided completely by passport.js documentation
+//on using local storage for authentication
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+
+var db = require();
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username." });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: "Incorrect password." });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+//Need to export the passport configurations
+module.exports = passport;
---
+++ models/user.js
@@ -0,0 +1,17 @@
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+      // The username cannot be null
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isUsername: true
+        }
+      },
+      // The password cannot be null
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    });
