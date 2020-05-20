654c521273f5008cdfb28709e2570af354572d16
Sean Baklini <sbaklini@unm.edu>
2020-05-19 18:59:32 -0600

    Ability for the deck title and category to make a post request to the server

--- public/js/deck.js
+++ public/js/deck.js
@@ -1,20 +1,46 @@
 //Going to need the functionality to add a new card and append more html
 //Going to need the functionality to add a new deck this would be a post request
+let titleInput = $("#title")
+let categoryInput = $("#category")
+let frontofCard;
+let backofCard;
+
+
 $(document).ready(function () {
     let addnewCard = () => {
-
         $('#add-card').on('click', () => {
             alert("card added")
             let addcardHTML = $('<div class="row"><form class="col s5"><div class="row"><div class="input-field col s6"><i class="material-icons prefix">code</i><textarea id="textarea1" class="materialize-textarea" placeholder="Front of Card"></textarea><label for="textarea1"></label></div></div></form><form class="col s5"><div class="row"><div class="input-field col s6"><i class="material-icons prefix">code</i><textarea id="textarea1" class="materialize-textarea" placeholder="Back of Card"></textarea><label for="textarea1"></label></div></div></form>')
             let containertoaddCard = $('.add-input');
-
             containertoaddCard.append(addcardHTML);
+        })
+    }
 
+    let postDeck = () => {
+        $('#add-deck').on('submit', (event) => {
+            event.preventDefault();
+            let deck = {
+                title: titleInput.value(),
+                category: categoryInput.value()
+            }
+            addDeck(deck);
         })
 
+    }
+
+    let addDeck = (deck) => {
+        $.post("/api/posts/", deck, () => {
+            window.location.href = "/deck";
+        })
+    }
+
+    let grabCards = () => {
 
     }
 
     addnewCard();
+    postDeck();
+
+
 
 })
