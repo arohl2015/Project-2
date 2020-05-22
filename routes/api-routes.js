f24aa2370ecb9ee5df2aa254cc98bee8f9d894d8
Sean Baklini <sbaklini@unm.edu>
2020-05-22 16:46:44 -0600

    Team debuggin routes, get routes correct

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,40 +6,41 @@ module.exports = function (app) {
     // Using the passport.authenticate middleware with our local strategy
     // we want to make sure the user with valid login credentials gets sent to the right page
     app.post("/api/login", passport.authenticate("local"), function (req, res) {
-        res.json(req.user);
+        res.json("/account");
     });
     // Route for signing up a user.
     // if the user is created successfully, proceed to log the user in or send back an error
-    app.post("/api/signup", function(req, res) {
-      console.log(req.body);
-      db.User.create({
-        email: req.body.email,
-        password: req.body.password
-      }).then(function() {
-        res.redirect(307, "/api/login");
-      }).catch(function(err) {
-        console.log(err);
-        res.json(err);
-      });
+    app.post("/api/signup", function (req, res) {
+        console.log(req.body);
+        db.User.create({
+            email: req.body.email,
+            password: req.body.password
+        }).then(function () {
+            res.redirect(307, "/api/login");
+        }).catch(function (err) {
+            console.log(err);
+            res.json(err);
+        });
     });
     // Route that is used to log the user out
-    app.get("/logout", function(req, res) {
-      req.logout();
-      res.redirect("/");
+    app.get("/logout", function (req, res) {
+        req.logout();
+        res.redirect("/");
     });
-      // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", function(req, res) {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    }
-    else {
-      // Send back the user's email and id
-      res.json({
-        email: req.user.email,
-        id: req.user.id
-      });
-    };
+    // Route for getting some data about our user to be used client side
+    app.get("/api/user_data", function (req, res) {
+        if (!req.User) {
+            // The user is not logged in, send back an empty object
+            res.json({});
+        }
+        else {
+            // Send back the user's email and id
+            res.json({
+                email: req.user.email,
+                id: req.user.id
+            });
+        }
+    });
     //Grab all decks
     app.get("/api/decks", function (req, res) {
         db.Deck.findAll({}).then(function (dbDeck) {
@@ -62,7 +63,7 @@ module.exports = function (app) {
     app.get("/api/cards/:DeckId", function (req, res) {
         db.Card.findAll({
             where: {
-                DeckID: req.params.DeckId
+                DeckId: req.params.DeckId
             },
 
         }).then(function (cards) {
@@ -70,13 +71,16 @@ module.exports = function (app) {
         })
     })
     //This will allow us to determine who the deck is created by
-    app.get("/api/decks/name", function (req, res) {
+    app.get("/api/decks/name/:id", function (req, res) {
         db.Deck.findAll({
+
+
             include: [{
                 model: db.User,
-                attributes: ['username'],
+                attributes: ['email'],
                 where: {
-                    userId: db.Sequelize.col('User.id')
+                    userId: db.Sequelize.col('User.id'),
+                    DeckId: req.params.id
                 }
             }],
         }).then(function (result) {
@@ -119,25 +123,15 @@ module.exports = function (app) {
         })
     }
     )
-    // Update deck with corresponding ID (editting specific deck)
-    app.put("/api/decks/update/:id", function (req, res) {
-        db.Deck.update({
-            title: req.body.title,
-            body: req.body.body,
-
-        }, {
-            where: {
-                id: req.params.id
-            }
-
     app.post("/api/decks", function (req, res) {
         db.Deck.create(req.body).then(function (dbDeck) {
             res.json(dbDeck);
-        });
-        }).then(function () {
-            res.json("Update")
         })
+            .then(function () {
+                res.json("Update")
+            })
     })
+
     //Update card within deck based off of id
     app.put("/api/card/update/:id", function (req, res) {
         db.Card.update({
@@ -154,7 +148,7 @@ module.exports = function (app) {
     })
 
     //delete from user decks (should this be a delete request to user decks instead of deck?)
-    app.delete("/api/decks/delete/:id", function (req, res) {
+    app.delete("/api/decks/decks/:id", function (req, res) {
         db.Deck.destroy({
             where: {
                 id: req.params.id
@@ -164,7 +158,7 @@ module.exports = function (app) {
         });
     });
     //Deletes the card from the corresponding deck (userdeck)
-    app.delete("/api/card/delete/:id", function (req, res) {
+    app.delete("/api/card/decks/:id", function (req, res) {
         db.Card.destroy({
             where: {
                 id: req.params.id
@@ -213,4 +207,4 @@ module.exports = function (app) {
     //     });
     // });
 
-};
+}
